41107f7c4011f7d835bf69c1dce6e75694d8c8d5
Lakshmi nainitha <15nainitha@gmail.com>
2026-04-05 21:24:55 +0530

    Fix Express 5 wildcard routing error

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use('/api/complaints', complaintRoutes);
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 app.listen(PORT, () => {
